e587ecb8fb5897613a352ca2a50de16eea593a54
+ <+>
2021-06-30 19:09:52 -0400

    Complete

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Write your code here!
+
+/*
+const body = document.body
+body.append("Sup Son", "Doe")
+
+const div = document.createElement("div")
+div.textContent = "LaaziMate" 
+div.innerText ="Laaz"
+
+
+div.innerHTML ="<em>gird</em>"
+
+const strong = document.createElement("strong")
+strong.innerText="HelloBrother"
+div.append(strong)
+
+body.append(div)
+
+
+const para = document.createElement("p")
+body.append(para)
+*/
+
+const mainHead = document.querySelector("#main")
+mainHead.remove()
+
+
+const newHeader = document.getElementById('victory')
+
+
+//const newHeader = document.createElement("h1")
+//const newHeader 
+
+
+
+//headLocation =  document.body.h1
+//headLocation.append("YOUR-NAME is the champion")
+//newHeader.append("YOUR-NAME is the champion")
+
+const headText = document.querySelector('h1#victory')
+headText.append("YOUR-NAME is the champion")
+
